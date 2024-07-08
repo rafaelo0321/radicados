57ab19cd396fb27e9efd10fc8a63e3b57a69f2e2
rafaelo0321 <rafaninho21@gmail.com>
2024-07-08 13:10:38 -0500

    Integracion de metodos de dependencias

--- src/main/java/com/superradicado/radicados/seguridad/SecurityConfigurations.java
+++ src/main/java/com/superradicado/radicados/seguridad/SecurityConfigurations.java
@@ -14,6 +14,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -38,10 +39,9 @@ public class SecurityConfigurations {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(
                         auth -> auth
-                                //.requestMatchers(new AntPathRequestMatcher("/auth/**"), HttpMethod.POST).permitAll()
-                                .requestMatchers("/auth/**").permitAll()
+                                .requestMatchers(new AntPathRequestMatcher("/auth/**")).permitAll()
                                 .requestMatchers("/h2-console/**").permitAll()
-                                .requestMatchers("/api/v1/**").permitAll()
+                                .requestMatchers(new AntPathRequestMatcher("/api/v1/**")).permitAll()
                                 .requestMatchers("/swagger-ui/**").permitAll()
                                 .requestMatchers("/v3/api-docs/**").permitAll()
                                 .anyRequest().authenticated())
